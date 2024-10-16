791f4c9f5289d413da8c56faf9958b2a18ff2ff5
jcordry <julien.cordry@gmail.com>
2024-10-16 12:31:49 +0100

    Added dice rolls

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -2,8 +2,27 @@
 //
 
 #include <iostream>
+#include <set>
+#include <cstdlib>
+
+using namespace std;
+
+int die()
+{
+	return (1 + rand() % 6);
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	srand(time(NULL));
+	set<int> rolls;
+	for (size_t iteration = 0; iteration < 12; iteration++)
+	{
+		rolls.insert(die());
+		for (auto& i : rolls)
+		{
+			cout << i << " ";
+		}
+		cout << endl;
+	}
 }
